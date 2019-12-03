556e7be1079454c1038d369e7e719c96d8688bcc
connor <connor.jaksik@gmail.com>
2019-12-03 16:14:05 -0700

    initial commit, created landing background

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,25 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import "./index.css"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="landing-wrapper">
+    <div className="space">
+
+      <div className="content-container">
+      <h1>My name is <br/>Connor Jaksik</h1>
+      <h1 className="right">I Make <br/>Digital Assets</h1>
+      <h1>It May Seem <br/> That My Head Is<br/> In The Clouds</h1>
+      <h1 className="right">That's Because <br/> It Is</h1>
+      <h1>I Also Climb <br/> Mountains</h1>
+
+      </div>
+    </div>
+
+<Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
