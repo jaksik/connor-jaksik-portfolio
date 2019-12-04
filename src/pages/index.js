32fb1df9bea5224a199199394583ddcd747fd569
connor <connor.jaksik@gmail.com>
2019-12-04 16:02:10 -0700

    added clouds

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,22 +7,65 @@ import Img from "gatsby-image"
 import Two from "../components/image-two"
 import SEO from "../components/seo"
 import "./index.css"
+import "./clouds.css"
 
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <div className="landing-wrapper">
-      <div className="space">
-        <div className="content-container">
-          <h1>I Architect<br /> Software</h1>
-          <h1 className="right">And Create <br />Digital Assets</h1>
-          <h1>It May Seem <br /> Like My Head Is<br /> In The Clouds</h1>
-          <h1 className="right">That's Because <br /> It Is</h1>
+      <div class="stars"></div>
+      <div class="twinkling"></div>
+
+
+      <div style={{ position: `absolute`, zIndex: `1`, width: `100%` }}>
+        <div className="space">
+          <div className="content-container">
+            <h1>I Architect<br /> Software</h1>
+            <h1 className="right">And Create <br />Digital Assets</h1>
+            <h1>It May Seem <br /> Like My Head Is<br /> In The Clouds</h1>
+            <h1 className="right">That's Because <br /> It Is</h1>
+          </div>
+          <div className="cloud-container">
+            <Img fluid={data.mountain.childImageSharp.fluid} className="landing-image" style={{position:`absolute`, width: `100%` }}/>
+            <div className="clouds"></div>
+          </div>
+        </div>
+        <div className="ground">
+          <Img fluid={data.background.childImageSharp.fluid} style={{ marginTop: `-100px`, zIndex: `-1`, position: `absolute`, width: `100%` }} />
+          <Img fluid={data.background.childImageSharp.fluid} style={{ position: `absolute`, width: `100%`, height: `100%`, marginTop: `100%` }} />
+          {/* <Img fluid={data.background.childImageSharp.fluid} style={{ width: `100%`, height: `100vh`, marginTop: `100vh` }} /> */}
+          <div className="content-container">
+            <h1>I am<br /> Connor Jaksik</h1>
+            <p>
+              For over a decade before becoming a software engineer I was a Skilled Tradesman.
+              I completed hundereds of carpentry and welding projects in several different states and countries.
+              I had the opportunity to build projects for many amazing people and lead teams of incredibly hard working individuals.
+            <br />
+              <br />
+              This developed my invaluable work ethic of no compromises and only producing the highest quality work.
+              I became obsessed with software and digital assets as I began to realize their unique qualities of being able to
+              reach any one in the world at any time and their ability to be replicated an infite number of times.
+            <br />
+              <br />
+              I believe that one produces their best work when their life is balanced with relationships and other passions.
+              That's why setting a side time to be away from my computer has become sacred to me. If I don't respond to an email quickly
+              it's most likely because I'm climbing a mountin or skiing with friends.
+            <br />
+              <br />
+              Dont be shy, send me a message, let's get coffee, and talk about something great that we can create together.
+          </p>
+            <p style={{ textAlign: `right` }}>
+              <span style={{ paddingRight: `40px` }}>- Connor Jaksik</span>
+              <br />
+              <span>Boulder, Colorado</span>
+            </p>
+          </div>
         </div>
       </div>
-      <Image />
+
+
     </div>
-    <div className="landing-content" style={{ position: `relative` }}>
+    {/* <div className="landing-content" style={{ position: `relative` }}>
       <Img fluid={data.background.childImageSharp.fluid} style={{ position: `absolute`, width: `100%`, height: `100vh` }} className="image-two" />
       <Img fluid={data.background.childImageSharp.fluid} style={{ position: `absolute`, width: `100%`, height: `100vh`, marginTop: `-30vh`, zIndex: `-50` }} />
       <Img fluid={data.background.childImageSharp.fluid} style={{ position: `absolute`, width: `100%`, height: `100vh`, marginTop: `100vh` }} />
@@ -33,30 +76,7 @@ const IndexPage = ({ data }) => (
 
       <div style={{ position: `absolute`, zIndex: `100`, width: `100%` }}>
         <div className="content-container" style={{ position: `relative` }}>
-          <h1>I am<br /> Connor Jaksik</h1>
-          <p>
-            For over a decade before becoming a software engineer I was a Skilled Tradesman.
-            I completed hundereds of carpentry and welding projects in several different states and countries.
-            I had the opportunity to build projects for many amazing people and lead teams of incredibly hard working individuals.
-            <br />
-            <br />
-            This developed my invaluable work ethic of no compromises and only producing the highest quality work.
-            I became obsessed with software and digital assets as I began to realize their unique qualities of being able to
-            reach any one in the world at any time and their ability to be replicated an infite number of times.
-            <br />
-            <br />
-            I believe that one produces their best work when their life is balanced with relationships and other passions.
-            That's why setting a side time to be away from my computer has become sacred to me. If I don't respond to an email quickly
-            it's most likely because I'm climbing a mountin or skiing with friends.
-            <br />
-            <br />
-            Dont be shy, send me a message, let's get coffee, and talk about something great that we can create together.
-          </p>
-          <p style={{ textAlign: `right` }}>
-            <span style={{ paddingRight: `55px` }}>- Connor Jaksik</span>
-            <br />
-            <span>Boulder, Colorado</span>
-          </p>
+        
           <form
             name="contact"
             method="post"
@@ -66,19 +86,12 @@ const IndexPage = ({ data }) => (
           >
 
             <h1 style={{ margin: `20px auto`, textAlign: `center`, padding: `0`, color: `black` }}>Let's Do This</h1>
-
             <input type="hidden" name="bot-field" />
-
             <input type="hidden" name="form-name" value="contact" />
-
             <input style={{ display: `none` }} type="text" /><br />
-
             <input required className="form-input" name="name" placeholder="First and Last Name*" type="text" /><br />
-
             <input required className="form-input" name="email" placeholder="Email*" type="email" /><br />
-
             <input className="form-input" name="phone" placeholder="Phone Number" type="number" /><br />
-
             <textarea className="form-input" name="message" placeholder="  Anything else we should know before your lesson?" type="textarea" /><br />
 
             <button className="submit-button">Send It!</button>
@@ -86,7 +99,7 @@ const IndexPage = ({ data }) => (
           </form>
         </div>
       </div>
-    </div>
+    </div> */}
   </Layout>
 )
 
@@ -94,6 +107,13 @@ export default IndexPage
 
 export const query = graphql`
   query {
+    mountain: file(relativePath: { eq: "mountain-top.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 2000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
     background: file(relativePath: { eq: "att.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 2000) {
@@ -102,6 +122,13 @@ export const query = graphql`
       }
     }
     trees: file(relativePath: { eq: "river.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 2000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    clouds: file(relativePath: { eq: "cloud.png" }) {
       childImageSharp {
         fluid(maxWidth: 2000) {
           ...GatsbyImageSharpFluid
---
+++ src/pages/test.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./test.css"
+
+const TestPage = () => (
+    <div>
+        <div class="stars"></div>
+        <div class="twinkling"></div>
+    </div>
+)
+
+export default TestPage;
